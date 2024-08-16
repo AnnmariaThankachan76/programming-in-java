0baa2a0f839ee8ecb558b114e0e7401447e4f01e
Annmaria Thankachan <165566292+AnnmariaThankachan76@users.noreply.github.com>
2024-08-17 04:10:29 +0530

    Add files via upload

---
+++ Expression.java
@@ -0,0 +1,10 @@
+
+public class Expression 
+{
+public static void main(String[] args)
+{
+	double n=4.0*(1-(1.0/3))+(1.0/5)-(1.0/9)-(1.0/11);
+	System.out.println("The output of the expression 4.0*(1-(1.0/3))+(1.0/5)-(1.0/9)-(1.0/11) is "+n);
+	
+}
+}
