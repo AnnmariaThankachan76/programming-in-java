699b3cde997a477ee809d5dd6a3fb27046f96698
Annmaria Thankachan <165566292+AnnmariaThankachan76@users.noreply.github.com>
2024-09-11 01:12:25 +0530

    Add files via upload

---
+++ Geometricshapes.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+abstract class Geometricshapes1 {
+abstract void  area();
+abstract void perimeter();
+}
+ class Rectangle extends Geometricshapes1{
+	 void area(float length,float breath) {
+		System.out.println("the area of the rectangle"+length*breath);
+		}
+	void perimeter(float length,float breath) {
+		System.out.println("the area of the rectangle"+2*(length*breath));	
+	}
+}
+ class Square extends Geometricshapes1{
+	 void  area(int side) {
+		 System.out.println("the area of the Square "+side*side);
+	 }
+	 void perimeter(int side) {
+		 System.out.println("the area of the Square "+(4*side));
+	 }
+}
+public class Geometricshapes{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the length and breath of rectangle ");
+		float length=sc.nextFloat();
+		float breath=sc.nextFloat();
+		Rectangle rectangle=new Rectangle();
+		rectangle.area(length,breath);
+		rectangle.perimeter(length,breath);
+		System.out.println("------------------------------- ");
+		System.out.println("Enter the sides of the square  ");
+		int sides=sc.nextInt();
+		Square square=new Square();
+		square.area(sides);
+		square.perimeter(sides);
+		
+	}
+	
+}
