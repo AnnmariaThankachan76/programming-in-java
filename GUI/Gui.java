abf236c0104d5406160ea4c138f8f9bc8fe2bd03
Annmaria Thankachan <165566292+AnnmariaThankachan76@users.noreply.github.com>
2024-11-11 02:57:34 +0530

    Add files via upload

---
+++ GUI/Gui.java
@@ -0,0 +1,12 @@
+package newo;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class Gui
+{public static void main (String[] args)
+{JFrame f=new JFrame ("calculator");
+f.setSize(500,400);
+f.setVisible(true);
+f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+}
